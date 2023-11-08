e42f2b628a7699192af1a3881bf03f5df58d08e9
Pablo03-dev <pablo.villegasa@alumnos.arcos.cl>
2023-11-08 11:14:05 -0300

    Cosasa diversas

---
+++ Assets/DelayedStart.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DelayedStart : MonoBehaviour
+{
+    [SerializeField] private GameObject countDownObject;
+    [SerializeField] private float countDowntime = 3f;
+
+    void Start()
+    {
+        countDownObject.SetActive(true);
+        StartCoroutine("StartDelay");
+    }
+
+    IEnumerator StartDelay()
+    {
+        Time.timeScale = 0;
+        float pauseTime = Time.realtimeSinceStartup + countDowntime;
+        while (Time.realtimeSinceStartup < pauseTime)
+        {
+            yield return 0;
+        }
+        countDownObject.SetActive(false);
+        Time.timeScale = 1;
+    }
+}
